b57e657743306f72ba1b84ee57b72ea21c62a929
Muhammad Majid <mmajidkamel@gmail.com>
2020-10-29 18:38:55 +0200

    server port to read from env

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ io.on('connection', socket => {
   });
 });
 
-server.listen(3030, () => {
+server.listen(process.env.PORT || 3030, () => {
   console.log('Server is up and running on 3000!');
 });
